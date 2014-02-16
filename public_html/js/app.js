0dd1dc7f021d597ab594cf6beaaf517a63e5224e
Anders Weijnitz <anders.weijnitz@gmail.com>
2014-02-16 21:52:33 +0100

    Refactored. Added geo API support

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,67 +1,29 @@
 
-function initialize() {
+function initializeMap(position) {
+
     var map;
-    var brooklyn = new google.maps.LatLng(59.3296, 18.069);
+    var centerPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    var MY_MAPTYPE_ID = 'custom_style';
-
-    var featureOpts = [
-        {
-            stylers: [
-                { hue: '#890000' },
-                { visibility: 'simplified' },
-                { gamma: 0.5 },
-                { weight: 0.5 }
-            ]
-        },
-        {
-            elementType: 'labels',
-            stylers: [
-                { visibility: 'off' }
-            ]
-        },
-        {
-            featureType: 'water',
-            stylers: [
-                { color: '#890000' }
-            ]
-        }
-    ];
-
-    var mapOptions = {
-        zoom: 12,
-        center: brooklyn,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-        },
-        mapTypeId: MY_MAPTYPE_ID
-    };
 
     map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+        conf.getMapOptions(centerPos));
 
     var styledMapOptions = {
         name: 'Custom Style'
     };
 
-    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+    var customMapType = new google.maps.StyledMapType(conf.featureOpts, styledMapOptions);
+    map.mapTypes.set(conf.getMapTypeId(), customMapType);
 
-    map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
 }
 
-function initialize_old() {
-    var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+function initialize() {
+    util.centerMap(initializeMap);
 }
+
 
 $(document).ready(function () {
 
-    // TODO: BEFORE LIVE, RESTRICT KEY ACCESS TO THE LIVE DOMAIN (REFERERS)
-    var apiKey = "AIzaSyCcr1fAQXMW24RIjzodbgyv61ztbGHz9Us";
 
 
     function loadScript() {
@@ -72,6 +34,6 @@ $(document).ready(function () {
         document.body.appendChild(script);
     }
 
-    window.onload = loadScript;
+    window.onload = loadScript; // Load map async
 
 }); // end onReady
